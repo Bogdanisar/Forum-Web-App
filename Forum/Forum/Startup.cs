34896a86c872ddf7e2b865726eb916f94f272b9c
Bogdan <burcea.bogdan.madalin@gmail.com>
2019-11-30 16:11:32 +0200

    Clean up startup.cs

--- Forum/Forum/Startup.cs
+++ Forum/Forum/Startup.cs
@@ -17,51 +17,6 @@ namespace Forum
             createAdminUserAndApplicationRoles();
         }
 
-        //private void createAdminUserAndApplicationRoles()
-        //{
-        //    ApplicationDbContext context = new ApplicationDbContext();
-
-        //    var roleManager = new RoleManager<IdentityRole>(new
-        //    RoleStore<IdentityRole>(context));
-
-        //    var UserManager = new UserManager<ApplicationUser>(new
-        //    UserStore<ApplicationUser>(context));
-
-        //    // Se adauga rolurile aplicatiei
-        //    if (!roleManager.RoleExists("Administrator"))
-        //    {
-        //        // Se adauga rolul de administrator
-        //        var role = new IdentityRole();
-        //        role.Name = "Administrator";
-        //        roleManager.Create(role);
-
-        //        // se adauga utilizatorul administrator
-        //        var user = new ApplicationUser();
-        //        user.UserName = "admin@admin.com";
-        //        user.Email = "admin@admin.com";
-
-        //        var adminCreated = UserManager.Create(user, "Administrator1!");
-        //        if (adminCreated.Succeeded)
-        //        {
-        //            UserManager.AddToRole(user.Id, "Administrator");
-        //        }
-        //    }
-
-        //    if (!roleManager.RoleExists("Editor"))
-        //    {
-        //        var role = new IdentityRole();
-        //        role.Name = "Editor";
-        //        roleManager.Create(role);
-        //    }
-
-        //    if (!roleManager.RoleExists("User"))
-        //    {
-        //        var role = new IdentityRole();
-        //        role.Name = "User";
-        //        roleManager.Create(role);
-        //    }
-        //}
-
         public struct UserInfo
         {
             public string role, email, parola;
@@ -94,12 +49,12 @@ namespace Forum
             {
                 if (!roleManager.RoleExists(info[i].role))
                 {
-                    // Se adauga rolul de administrator
+                    // se adauga rolul
                     var role = new IdentityRole();
                     role.Name = info[i].role;
                     roleManager.Create(role);
 
-                    // se adauga utilizatorul administrator
+                    // se adauga utilizatorul
                     var user = new ApplicationUser();
                     user.UserName = info[i].email;
                     user.Email = info[i].email;
